d0fb4b3f3f87e28d2a3ef3112ae4bcc3efad3f1c
tzlyh120 <40650562+tzlyh120@users.noreply.github.com>
2023-03-04 02:28:58 -0500

    pull test

--- IdeaProjects/com.atguigu.git-test/src/main/java/com/atguigu/git/GitTest.java
+++ IdeaProjects/com.atguigu.git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("mtmb4");
         System.out.println("master test");
         System.out.println("push test mtmb4");
+        System.out.println("pull test mtmb4");
 
     }
 }
